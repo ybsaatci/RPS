0fc474309350b1b11397ee68c7eda0ccc6a9c4ab
ybsaatci <yagiz.saatchi@gmail.com>
2022-09-02 21:32:02 -0700

    Add button to play rps

--- rps.js
+++ rps.js
@@ -1,5 +1,38 @@
 function getComputerChoice() {
     const rps = ["Rock", "Paper", "Scissors"];
     let choice = Math.floor(Math.random() * 3);
-    console.log(rps[choice]);
+    return rps[choice];
+}
+
+function playRps(playerSelection, computerSelection) {
+    if(playerSelection.toLowerCase() == "rock" && computerSelection == "Paper" ) {
+        return "You Lose!!";
+    } else if(playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
+        return "You Win!!";
+    } else if(playerSelection.toLowerCase() == "rock" && computerSelection == "Rock") {
+        return "Draw !!";
+    } else if(playerSelection.toLowerCase() == "paper" && computerSelection == "Rock") {
+        return "You Win!!";
+    } else if(playerSelection.toLowerCase() == "paper" && computerSelection == "Paper") {
+        return "Draw !!";
+    } else if(playerSelection.toLowerCase() == "paper" && computerSelection == "Scissors") {
+        return "You lose!";
+    } else if(playerSelection.toLowerCase() == "scissors" && computerSelection == "Rock") {
+        return "You lose!!";
+    } else if(playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper") {
+        return "You Win!!";
+    } else {
+        return "Draw!!";
+    }
+}
+
+
+
+
+function game() {
+    for(let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Pick between rock, paper, scissors", "rock");
+        const computerSelection = getComputerChoice();
+        console.log(playRps(playerSelection, computerSelection));
+    }
 }
